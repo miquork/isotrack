b1be77fa7cce33c48ef2809bb45edb6850ec1492
Mikko Voutilainen <voutila@Mikkos-MacBook-Air.local>
2025-06-25 13:58:21 +0300

    Stash compareCorrFact.C

--- compareCorrFact.C
+++ compareCorrFact.C
@@ -13,7 +13,8 @@ void parseSunanda();
 void compareCorrFacts(string file1, string file2,
 		      string name1="file1", string name2="filel2",
 		      string title = "IsoTrack", string name="",
-		      double ymin=0.8, double ymax=1.2, bool addDI=true) {
+		      double ymin=0.8, double ymax=1.2, bool addDI=true,
+		      bool drawErr = true) {
 
   cout << "Running compareCorrFact(\""<<file1<<",\""<<file2<<"\")..."
        << endl << flush;
@@ -44,13 +45,13 @@ void compareCorrFacts(string file1, string file2,
   TLegend *leg = tdrLeg(0.40,0.90-(addDI ? 5 : 4)*0.05,0.65,0.90);
   TLegend *leg2 = tdrLeg(0.40,0.15,0.65,0.15+3*0.05);
 
-  TH1D *he = (TH1D*)fe->Get("h_di"); assert(he);
-  TH1D *ho = (TH1D*)fo->Get("h_di"); assert(ho);
-  TH1 *hr = (TH1D*)ho->Clone(Form("hr%s%s",cf1,cf2));
-  hr->Divide(he);
+  TH1D *he = (TH1D*)fe->Get("h_di"); assert(he || !addDI);
+  TH1D *ho = (TH1D*)fo->Get("h_di"); assert(ho || !addDI);
+  TH1 *hr = (ho ? (TH1D*)ho->Clone(Form("hr%s%s",cf1,cf2)) : 0);
+  if (hr && he) hr->Divide(he);
 
   // Patch the missing negative side by reflecting denominator to -|i#eta|
-  if (name=="abs") {
+  if (name=="abs" && hr && he) {
     for (int i = 1; i != hr->GetNbinsX()+1; ++i) {
       double ix = hr->GetBinCenter(i);
       if (ix<0 && hr->GetBinContent(i)==0) {
@@ -63,7 +64,7 @@ void compareCorrFacts(string file1, string file2,
     }
   }
 
-  if (addDI) leg->AddEntry(hr,"Depth independent","PLE");
+  if (addDI && hr) leg->AddEntry(hr,"Depth independent","PLE");
 
   TLine *l = new TLine();
   l->SetLineStyle(kDashed);
@@ -71,17 +72,24 @@ void compareCorrFacts(string file1, string file2,
   l->DrawLine(-29,1,29,1);
   l->SetLineStyle(kDotted);
   l->SetLineColor(kGray+1);
-  l->DrawLine(-15.5,0.8,-15.5,1.2);
-  l->DrawLine(+15.5,0.8,+15.5,1.2);
+  l->DrawLine(-16.5,ymin,-16.5,ymax);
+  l->DrawLine(+16.5,ymin,+16.5,ymax);
   l->SetLineColor(kGray+1);
-  l->DrawLine(-17.5,0.8,-17.5,1.2);
-  l->DrawLine(+17.5,0.8,+17.5,1.2);
+  l->DrawLine(-18.5,ymin,-18.5,ymax);
+  l->DrawLine(+18.5,ymin,+18.5,ymax);
+  l->SetLineStyle(kSolid);
+  l->DrawLine(-27.5,ymin,-27.5,ymax);
+  l->DrawLine(+27.5,ymin,+27.5,ymax);
+  
   
   // Calculate correction ratios per depth
   const int ndepth = 10;
+  //int color[ndepth] =
+  //{kBlack, kRed, kGreen+2, kBlue, kMagenta+1,
+  // kOrange+1,kCyan+2,kGray+2, 1,1};
   int color[ndepth] =
-    {kBlack, kRed, kGreen+2, kBlue, kMagenta+1,
-     kOrange+1,kCyan+2,kGray+2, 1,1};
+    {kBlack, kBlue, kOrange+1, kGreen+1, kRed,
+     kYellow+1, kOrange+3, kGray+2};
   int marker[ndepth] =
     {1, kOpenTriangleDown, kOpenSquare, kOpenCircle, kOpenTriangleUp,
      kOpenStar, kOpenDiamond, kOpenCross, 1,1};
@@ -109,18 +117,25 @@ void compareCorrFacts(string file1, string file2,
       }
     }
 
-    tdrDraw(hr,"Pz",marker[i],color[i], kSolid,-1,kNone,0, 0.8);
+    if (drawErr)
+      tdrDraw(hr,"Pz",marker[i],color[i], kSolid,-1,kNone,0, 0.8);
+    else
+      tdrDraw(hr,"HIST][",marker[i],color[i], kSolid,-1,kNone,0, 0.8);
     hr->SetLineWidth(2);
     if (i<5) leg->AddEntry(hr,Form("DD depth %d",i),"PLE");
     else leg2->AddEntry(hr,Form("DD depth %d (HE)",i),"PLE");
   }
 
   // Reference results from depth-independent case
-  if (addDI) {
-    tdrDraw(hr,"Pz",kFullCircle,kBlack, kSolid,-1,kNone,0, 0.8);
+  if (addDI && hr) {
+    if (drawErr)
+      tdrDraw(hr,"Pz",kFullCircle,kBlack, kSolid,-1,kNone,0, 0.8);
+    else
+      tdrDraw(hr,"HIST][",kFullCircle,kBlack, kSolid,-1,kNone,0, 0.8);
     hr->SetLineWidth(2);
   }
-    
+
+  gPad->RedrawAxis();
   c1->SaveAs(Form("pdf/compareCorrFact_%s_vs_%s%s.pdf",cf1,cf2,cn));
 
   cout << "Done with compareCorrFact(\""<<file1<<",\""<<file2<<"\")."
@@ -167,12 +182,14 @@ void compareCorrFact() {
   // v31: v28/29 without gains
   // v32: v28/29 without tresholds
   // v33: v28/29 with 3x2 approach
+  /*
   compareCorrFacts("rootfiles/CorrFact_Sunanda_24CDEFGHI.root",
-		   "rootfiles/CorrFact_lxplus_v27_24CDEFGHI.root",
+		   "rootfiles/CorrFact_lxplus_v29_24CDEFGHI.root",
 		   "Sunanda","Mikko","IsoTrack: teams","_zoomout",0.,2.,false);
   compareCorrFacts("rootfiles/CorrFact_Sunanda_24CDEFGHI.root",
-		   "rootfiles/CorrFact_lxplus_v27_24CDEFGHI.root",
+		   "rootfiles/CorrFact_lxplus_v29_24CDEFGHI.root",
 		   "Sunanda","Mikko","IsoTrack: teams","_zoomin",0.8,1.3,false);
+  */
   /*
   compareCorrFacts("rootfiles/CorrFact_lxplus_v13_24CDEFGHI.root",
 		   "rootfiles/CorrFact_lxplus_v14_24CDEFGHI.root",
@@ -183,13 +200,76 @@ void compareCorrFact() {
 		   "rootfiles/CorrFact_hybrid_lxplus_v14_24CDEFGHI.root",
 		   "signed","abs","IsoTrack: hybrid correction",
 		   "",0.9,1.15);
-
-  compareCorrFacts("rootfiles/CorrFact_odd_hybrid_lxplus_v14_24CDEFGHI.root",
-		   "rootfiles/CorrFact_even_hybrid_lxplus_v14_24CDEFGHI.root",
-		   "odd","even","IsoTrack: even vs odd",
+  */
+  // v14->v28
+  /*
+  compareCorrFacts("rootfiles/CorrFact_odd_hybrid_lxplus_v28_24CDEFGHI.root",
+		   "rootfiles/CorrFact_even_hybrid_lxplus_v28_24CDEFGHI.root",
+		   "odd","even","IsoTrack hybrid 24CDEFGHI: even vs odd",
 		   "",0.9,1.15);
   */
 
+  /*
+  compareCorrFacts("solveIsoTrackV2_24MCNOPU.root",
+		   "solveIsoTrackV2_24MCPU.root",
+		   "noPU","PU","IsoTrackV2 MC",
+		   "",0.65,1.25, false, false);
+  
+  compareCorrFacts("solveIsoTrackV2_24MCPU.root",
+		   "solveIsoTrackV2_24CDEFGHI.root",
+		   "MCPU","Data","IsoTrackV2 24CDEFGHI",
+		   "",0.45,2.45, false, false);
+
+  compareCorrFacts("solveIsoTrackV2_24CDEFGHI.root",
+		   "rootfiles/CorrFact_lxplus_v29_24CDEFGHI.root",
+		   "3x5","v29","IsoTrack 24CDEFGHI",
+		   "",0.0,1.7, false, false);
+
+  compareCorrFacts("rootfiles/CorrFact_Sunanda_24CDEFGHI.root",
+		   "solveIsoTrackV2_24CDEFGHI.root",
+		   "Sunanda","MikkoV2","IsoTrack 24CDEFGHI",
+		   "",0.6,2.0, false, false);
+  */
+
+  compareCorrFacts("rootfiles/CorrFact_Sunanda_24CDEFGHI_v1_nogain.root",
+		   "rootfiles/CorrFact_Sunanda_24CDEFGHI.root",
+		   "SunandaV1","SunandaV2","IsoTrack 24CDEFGHI",
+		   //"",0.0,2.0, false, false);
+		   //"",0.8,1.4, false, false);
+		   "",0.95,1.05, false, false);
+
+  compareCorrFacts("rootfiles/CorrFact_Sunanda_24CDEFGHI.root",
+		   "rootfiles/CorrFact_lxplus_v33_24CDEFGHI.root",
+		   "SunandaV2","MikkoV2","IsoTrack 24CDEFGHI",
+		   //"",0.55,1.5, false, true);
+		   "",0.4,2.0, false, true);
+  /*
+  compareCorrFacts("rootfiles/CorrFact_lxplus_v33_24MCNOPU.root",
+		   "solveIsoTrackV2_24MCNOPU.root",
+		   "v33","3x5","IsoTrack(V2) MCNOPU",
+		   "",0.65,1.25, false, false);
+  */
+  /*
+  compareCorrFacts("rootfiles/CorrFact_lxplus_v33_24MCPU.root",
+		   "rootfiles/CorrFact_lxplus_v33_24CDEFGHI.root",
+		   "MCPU","Data","IsoTrack (Sunanda-style) v33",
+		   "",0.,2.5, false, false);
+  compareCorrFacts("rootfiles/CorrFact_lxplus_v33_24MCNOPU.root",
+		   "rootfiles/CorrFact_lxplus_v33_24MCPU.root",
+		   "MCNOPU","MCPU","IsoTrack (Sunanda-style) v33",
+		   "",0.5,1.5, false, false);
+  */
+  /*
+  compareCorrFacts("rootfiles/solveIsoTrackV2_lxplus_v3_24MCPU.root",
+		   "rootfiles/solveIsoTrackV2_lxplus_v3_24CDEFGHI.root",
+		   "MCPU","Data","IsoTrack (3x5 V2) v3",
+		   "_3x5V2",0.,2.5, false, false);
+  compareCorrFacts("rootfiles/solveIsoTrackV2_lxplus_v3_24MCNOPU.root",
+		   "rootfiles/solveIsoTrackV2_lxplus_v3_24MCPU.root",
+		   "MCNOPU","MCPU","IsoTrack (3x5 V2) v3",
+		   "_3x5V2",0.5,1.5, false, false);
+  */
+  
   /*
   compareCorrFacts("rootfiles/CorrFact_lxplus_v15_24CDEFGHI.root",
 		   "rootfiles/CorrFact_lxplus_v14_24CDEFGHI.root",
@@ -214,7 +294,10 @@ void parseSunanda() {
   TH1D *h0 = (TH1D*)h->Clone("hf_dd_0"); h0->Reset();
   
   // For Sunanda's results in text file format
-  ifstream fin("textfiles/24CDEFGHIEBS00corr.txt");
+  //ifstream fin("textfiles/24CDEFGHIEBS00corr.txt"); // buggy?
+  //ifstream fin("textfiles/24CDEFGHIEASO00corr.txt"); // fixed, but wrong "#5"
+  ifstream fin("textfiles/24CDEFGHIEAS00corr.txt"); // fixed, correct(?) "#4"
+  
 
   // Remove header
   char cline[1024];
